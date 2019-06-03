8e217c4441b8a02d4ed143d99115f7b02afdd74e
khjbest <rnjsguswl393@likelion.org>
2019-06-03 21:32:06 +0900

    190603

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -139,3 +139,8 @@ MEDIA_URL='/media/'
 import dj_database_url
 db_from_env=dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
